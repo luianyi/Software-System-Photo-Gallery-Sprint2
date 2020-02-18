f9abbb95245db694a26401add581b3d972e9aca7
luianyi <48133702+luianyi@users.noreply.github.com>
2020-02-18 09:33:07 -0800

    Automate location search to only show the pic on "MY phone" that were taken outside of capstone room

--- app/src/test/java/com/example/photogallery2/SearchUtilTest.java
+++ app/src/test/java/com/example/photogallery2/SearchUtilTest.java
@@ -141,4 +141,51 @@ public class SearchUtilTest
     }//end date search
 
 
+    //--------location search------------------// IL
+    @Test
+    public void LocationSearch()
+    {
+        //Given that there are two pictures, one taken at location
+        //
+        //
+        //
+        // one taken on January 20 2020, and one
+        //taken on January 22 2020, and the user searched for pictures between
+        //January 21 2020 and January 23 2020 (leaving the caption blank)...
+        Context c = getApplicationContext();
+        File f = null;
+        f = c.getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        try{
+            File.createTempFile("img1", ".JPEG", f);
+            File.createTempFile("img2", ".JPEG", f);
+        } catch (IOException e) {}
+        List captionListM = new ArrayList();
+        captionListM.add("Caption 1");
+        captionListM.add("Caption 2");
+        List dateListM = new ArrayList();
+        dateListM.add("Mon Jan 20 08:00:00 PST 2020");
+        dateListM.add("Wed Jan 22 08:30:00 PST 2020");
+        String get_caption = "";
+        String returnStartTime = "20200121_090909"; String returnEndTime = "20200123_090909";
+        String lat1 = "49.2503"; String lng1 = "-123.002500"; String lat2 = "49.2500"; String lng2 = "-123.002600";
+
+        //When Search() is called...
+        SearchUtil S = new SearchUtil();
+        Context context = getApplicationContext();
+        List result = S.Search(context,get_caption,returnStartTime,returnEndTime,lat1,lng1,lat2,lng2,captionListM,dateListM);
+
+        //Does it return only the picture taken on January 22?
+        List filenameListF = (List) result.get(0);
+        List captionListF = (List) result.get(1);
+        List dateListF = (List) result.get(2);
+        assertEquals(1, filenameListF.size()); //should be 1 file in the list
+        assertEquals(1, captionListF.size()); //should be 1 caption in the list
+        assertEquals(1, dateListF.size()); //should be 1 date in the list
+        //Verify content of lists is correct
+        assertEquals(true, filenameListF.get(0).toString().contains("img2"));
+        assertEquals("Caption 2", captionListF.get(0));
+        assertEquals("Wed Jan 22 08:30:00 PST 2020", dateListF.get(0));
+    }//end date search
+
+
 }//end SearchUtilTest
